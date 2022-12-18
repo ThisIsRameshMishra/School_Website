e4aa95c90a4948ecb500221ea1b3558706878478
Ramesh Mishra <rameshmishra0825@gmail.com>
2022-12-18 21:15:59 +0530

    Updated Admission page- added the header element

--- frontend/src/components/Main/Admission/Admission.jsx
+++ frontend/src/components/Main/Admission/Admission.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-
-
+import img from "../../../assets/images/img.jpg"
+import "./styles/admission.css"
 const Admission=()=>{
 return(
     <>
     <div className="header">
-        <div className="left">Hello</div>
-        <div className="right">
-        <img src=""/>
+        <div className="lefta"><div className="leftb">
+        <p className="h1_add">ADMISSION</p>
+        <p>-</p>
+        </div></div>
+        <div className="righta">
+        <img className="header_img" src={img}/>
         </div>
     </div>
-    <h1> Admissionss</h1>
+    
     </>
 )
 }
