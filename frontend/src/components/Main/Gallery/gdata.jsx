b01c33dd5b0fac27de317c1596bd870a5c5f6e3e
Ramesh Mishra <rameshmishra0825@gmail.com>
2023-01-29 13:41:25 +0530

    Updated about page

--- frontend/src/components/Main/Gallery/gdata.jsx
+++ frontend/src/components/Main/Gallery/gdata.jsx
@@ -2,6 +2,12 @@ import image1 from '../../../assets/images/img.jpg';
 import i1 from "../../../assets/SchoolActivites/img9.jpeg";
 import i2 from "../../../assets/SchoolActivites/img10.jpeg";
 import i3 from "../../../assets/SchoolActivites/img11.jpeg";
+import rd1 from "../../../assets/SchoolActivites/RD1.jpeg";
+import rd2 from "../../../assets/SchoolActivites/RD2.jpeg";
+import rd3 from "../../../assets/SchoolActivites/RD3.jpeg";
+import rd4 from "../../../assets/SchoolActivites/RD4.jpg";
+import SJ1 from "../../../assets/SchoolActivites/SJ1.jpeg";
+import SJ2 from "../../../assets/SchoolActivites/SJ2.jpeg";
 
 const gdata = [
 	{
@@ -13,7 +19,7 @@ const gdata = [
 
 	{
 		text: 'ANNUAL DAY',
-		img: [i1,i2,i3],
+		img: [rd1,rd2,rd3,rd4],
 	},
 	{
 		text: 'SPORTS DAY',
@@ -36,7 +42,7 @@ const gdata = [
 	{
 		text: 'CELEBRATIONS',
 		img: [
-			'https://images.unsplash.com/photo-1581456495146-65a71b2c8e52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80',
+			SJ1,SJ2
 		],
 	},
 ];
