cafe3abb3b11a3127fece5e2583ec802cfccb5c8
Mangesh <63477318+mangeshkchauhan@users.noreply.github.com>
2023-01-29 19:26:29 +0530

    refactor: code refactor

--- frontend/src/components/Main/Home/Messagebox.jsx
+++ frontend/src/components/Main/Home/Messagebox.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 import i1 from '../../../assets/SchoolActivites/img9.jpeg';
 
 const Messagebox = () => {
-	return (
-		<div className='msgbox'>
-			<div className='msg-content'>
-				<h3 className='block-title'>Message from the Director's Desk</h3>
-				<div className='intro'>
-					<p className='message'>
-						Dear Parents / Guardians, I took forward to extending a warm welcome to
-						your child on behalf of the faculty and staff of SN Public School. SN
-						Public is a school that is determined to help develop its students into
-						well-rounded citizens of the world. I am extremely proud of its academic
-						record, expansive campus and caring environment.
-					</p>
-					<div className='source'>
-						<div className='name-script'>Anita Mishra</div>
-						<div className='title'>Director</div>
-					</div>
-				</div>
-			</div>
-			<div className='figure'>
-				<img className='dir-img' src={i1} alt='' />
-			</div>
-		</div>
-	);
+  return (
+    <div className='msgbox'>
+      <div className='msg-content'>
+        <h3 className='block-title'>Message from the Director's Desk</h3>
+        <div className='intro'>
+          <p className='message'>
+            Dear Parents / Guardians, I took forward to extending a warm welcome
+            to your child on behalf of the faculty and staff of SN Public
+            School. SN Public is a school that is determined to help develop its
+            students into well-rounded citizens of the world. I am extremely
+            proud of its academic record, expansive campus and caring
+            environment.
+          </p>
+          <div className='source'>
+            <div className='name-script'>Anita Mishra</div>
+            <div className='title'>Director</div>
+          </div>
+        </div>
+      </div>
+      <div className='figure'>
+        <img className='dir-img' src={i1} alt='' />
+      </div>
+    </div>
+  );
 };
 
 export default Messagebox;
