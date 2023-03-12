c1c9fd756edafeabb2a0b174ccf25ed4d311df0c
Ramesh Mishra <rameshmishra0825@gmail.com>
2023-03-12 17:19:38 +0530

    Slight Modification

--- frontend/src/components/Main/Gallery/gdata.jsx
+++ frontend/src/components/Main/Gallery/gdata.jsx
@@ -2,48 +2,59 @@ import image1 from '../../../assets/images/img.jpg';
 import i1 from "../../../assets/SchoolActivites/img9.jpeg";
 import i2 from "../../../assets/SchoolActivites/img10.jpeg";
 import i3 from "../../../assets/SchoolActivites/img11.jpeg";
-import rd1 from "../../../assets/SchoolActivites/RD1.jpeg";
-import rd2 from "../../../assets/SchoolActivites/RD2.jpeg";
-import rd3 from "../../../assets/SchoolActivites/RD3.jpeg";
-import rd4 from "../../../assets/SchoolActivites/RD4.jpg";
+import i19 from "../../../assets/SchoolActivites/img19.jpeg";
+import i20 from "../../../assets/SchoolActivites/img20.jpeg";
+import i21 from "../../../assets/SchoolActivites/img21.jpeg";
+import i22 from "../../../assets/SchoolActivites/img22.jpeg";
+import i23 from "../../../assets/SchoolActivites/img23.jpeg";
+import i24 from "../../../assets/SchoolActivites/img24.jpeg";
+import i25 from "../../../assets/SchoolActivites/img25.jpeg";
+import i26 from "../../../assets/SchoolActivites/img26.jpeg";
+import i27 from "../../../assets/SchoolActivites/img27.jpeg";
+import i28 from "../../../assets/SchoolActivites/img28.jpeg";
+import i29 from "../../../assets/SchoolActivites/img29.jpeg";
+import i30 from "../../../assets/SchoolActivites/img30.jpeg";
+import i31 from "../../../assets/SchoolActivites/img31.jpeg";
 import SJ1 from "../../../assets/SchoolActivites/SJ1.jpeg";
 import SJ2 from "../../../assets/SchoolActivites/SJ2.jpeg";
+import c1 from "../../../assets/SchoolActivites/sn11_radhakrishna.jpeg";
+import sj3 from "../../../assets/SchoolActivites/sn14_opening.jpeg";
+import ed1 from "../../../assets/SchoolActivites/sn12_computer.jpeg"
+import c2 from "../../../assets/SchoolActivites/Holi/holi1.jpeg";
+import c3 from "../../../assets/SchoolActivites/Holi/holi2.jpeg";
+import c4 from "../../../assets/SchoolActivites/Holi/holi3.jpeg";
+import c5 from "../../../assets/SchoolActivites/Holi/holi4.jpeg";
+import c6 from "../../../assets/SchoolActivites/Holi/holi5.jpeg";
+
 
 const gdata = [
 	{
-		text: 'NEW NORMAL',
+		text: 'SOCIAL ACTIVITES',
 		img: [
-			i1,i2,i3
+			i1,i2,i3,i19,i21,i22,i23,i24,i25,i26,i27,i28,i29,i30,i31
 		],
 	},
 
 	{
 		text: 'ANNUAL DAY',
-		img: [rd1,rd2,rd3,rd4],
-	},
-	{
-		text: 'SPORTS DAY',
-		img: [
-			'https://images.unsplash.com/photo-1581456495146-65a71b2c8e52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80',
-		],
-	},
-	{
-		text: 'EDUCATIONAL DAY',
-		img: [
-			'https://images.unsplash.com/photo-1581456495146-65a71b2c8e52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80',
-		],
-	},
-	{
-		text: 'SCHOOL ACTIVITES',
-		img: [
-			'https://images.unsplash.com/photo-1581456495146-65a71b2c8e52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80',
-		],
+		img: [SJ1,SJ2,sj3],
 	},
 	{
 		text: 'CELEBRATIONS',
 		img: [
-			SJ1,SJ2
+			c1,c2,c3,c4,c5,c6
 		],
 	},
+	{
+		text: 'FUN DAY',
+		img: [i26,i27,i28,i29,i30,i31],
+	},
+	{
+		text: 'EDUCATIONAL DAY',
+		img: [
+			ed1
+		],
+	},
+	
 ];
 export default gdata;
--- frontend/src/components/Main/Home/Messagebox.jsx
+++ frontend/src/components/Main/Home/Messagebox.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
-import i1 from '../../../assets/SchoolActivites/img9.jpeg';
+import React from "react";
+import i1 from "../../../assets/SchoolActivites/sn13_directors.jpeg";
 
 const Messagebox = () => {
   return (
-    <div className='msgbox'>
-      <div className='msg-content'>
-        <h3 className='block-title'>Message from the Director's Desk</h3>
-        <div className='intro'>
-          <p className='message'>
-            Dear Parents / Guardians, I took forward to extending a warm welcome
-            to your child on behalf of the faculty and staff of SN Public
-            School. SN Public is a school that is determined to help develop its
-            students into well-rounded citizens of the world. I am extremely
-            proud of its academic record, expansive campus and caring
-            environment.
-          </p>
-          <div className='source'>
-            <div className='name-script'>Anita Mishra</div>
-            <div className='title'>Director</div>
+    <>
+      <section className="director-section">
+        <div className="dir-container">
+          <div className="row">
+            <div className="direc-image">
+              <img className="diri-img" src={i1} />
+            </div>
+            <div className="dir-msg">
+              <h2 className="director-heading">Director's Message</h2>
+              <h5 className="quote">
+                "There will always be a reason why you meet people. Either you
+                need them to change your life or you're the one that will change
+                theirs"
+              </h5>
+              <p className="msg1">
+                Welcome to our new school, where learning is an adventure and
+                curiosity is celebrated! We are excited to offer a dynamic and
+                engaging educational experience for students of all ages. Our
+                school is designed to meet the needs of today's learners, with
+                state-of-the-art facilities, modern technology, and a range of
+                extracurricular activities to foster creativity and growth. We
+                are committed to providing a safe and inclusive learning
+                environment that encourages critical thinking, problem-solving,
+                and collaboration.
+              </p>
+              <p className="msg2">
+                At our school, students will benefit from a diverse and highly
+                qualified team of educators who are passionate about teaching
+                and dedicated to helping each child reach their full potential.
+                Our curriculum is designed to meet the highest academic
+                standards while also promoting social-emotional learning and
+                character development.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='figure'>
-        <img className='dir-img' src={i1} alt='' />
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
