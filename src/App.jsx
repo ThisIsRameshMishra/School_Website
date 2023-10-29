ca8144192ac0af9228567eacd5ce8322d38a4e3a
Ramesh Mishra <rameshmishra0825@gmail.com>
2023-10-30 00:46:30 +0530

    Fixing 404 Error

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Pages from './components/Pages/Pages';
 import Nav, { NavPhone } from './components/Header/Nav';
 import Footer from './components/Footer/Footer';
-
+import { HashRouter } from 'react-router-dom';
 function App() {
 	const [menuOpen, setMenuOpen] = useState(false);
-
+	
 	return (
-		<Router history={hashHistory}>
+		<HashRouter >
 			<NavPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
 			<Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
 			<Pages />
 			<Footer />
-		</Router>
+		</HashRouter>
 	);
 }
 
