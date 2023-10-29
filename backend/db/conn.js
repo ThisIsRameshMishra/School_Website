d33ec5ebd7ef15b81f7c3cf252e23ac770939f58
Ramesh Mishra <rameshmishra0825@gmail.com>
2023-10-29 20:40:23 +0530

    Removed Nodejs backend

--- backend/db/conn.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose')
-const DB='mongodb+srv://student:0825ramesh@cluster0.ylr7abf.mongodb.net/mernstack?retryWrites=true&w=majority'
-mongoose.set("strictQuery", false);
-mongoose.connect(DB).then(()=>{
-    console.log("connect with mongo")
-    
-}).catch((err)=>console.log("failed"));
