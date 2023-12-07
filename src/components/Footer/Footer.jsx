7c814397226c81673d4824b5af72c3c048e8ec41
Mangesh Kumar <mangesh@fountane.com>
2023-12-07 22:45:10 +0530

    checking router

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => {
           <div className='bottom-footer-list-item'>ABOUT US</div>
           <div className='bottom-footer-list-item'>ADMISSION</div>
           <div className='bottom-footer-list-item'>
-            REACH US<span className='text-sm'>1.0.0</span>
+            REACH US<span className='text-sm'>1.0.1</span>
           </div>
         </div>
       </div>
--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,5 +1,7 @@
+/** @format */
+
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import Home from '../Main/Home/Home';
 import About from '../Main/About/About';
 import Contact from '../Main/Contact/Contact';
@@ -8,16 +10,17 @@ import Admission from '../Main/Admission/Admission';
 import NotFound from '../Main/NotFound/NotFound';
 
 const Pages = () => {
-	return (
-		<Routes>
-			<Route path='/' element={<Home />} />
-			<Route path='/about' element={<About />} />
-			<Route path='/contact' element={<Contact />} />
-			<Route path='/gallery' element={<Gallery />} />
-			<Route path='/admission' element={<Admission />} />
-			<Route path='*' element={<NotFound />} />
-		</Routes>
-	);
+  const location = useLocation();
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/gallery' element={<Gallery />} />
+      <Route path='/admission' element={<Admission />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default Pages;
