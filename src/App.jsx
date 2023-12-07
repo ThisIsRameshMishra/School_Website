db7aae7cbbe4b4baf9b88436496bac653b50e881
Mangesh Kumar <mangesh@fountane.com>
2023-12-07 22:16:48 +0530

    feat: fixed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+/** @format */
+
+import React, {useState} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Pages from './components/Pages/Pages';
-import Nav, { NavPhone } from './components/Header/Nav';
+import Nav, {NavPhone} from './components/Header/Nav';
 import Footer from './components/Footer/Footer';
-import { HashRouter } from 'react-router-dom';
 function App() {
-	const [menuOpen, setMenuOpen] = useState(false);
-	
-	return (
-		<HashRouter >
-			<NavPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-			<Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-			<Pages />
-			<Footer />
-		</HashRouter>
-	);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <Router>
+      <NavPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Pages />
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
