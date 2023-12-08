29f4777542b6a4a2d64bd2f95477b36ba608b9ef
Ramesh Mishra <rameshmishra0825@gmail.com>
2023-12-08 14:45:47 +0530

    About page content change

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -1,116 +1,142 @@
 import React from "react";
-import img from "../../../assets/images/admission.jpg";
 import vis from "../../../assets/SchoolActivites/Vission.jpg";
 import "./styles/about.scss";
+const img =
+  "https://ik.imagekit.io/snpublicschoolara/images/home2.jpeg?updatedAt=1702019189257";
 const About = () => {
   return (
     <div className="aboutpage">
-    <h1 className="aboutschool">ABOUT SN PUBLIC SCHOOL</h1>
-    <h1 className="aboutus">About Us</h1>
-    <hr className="line"/> 
+      <h1 className="aboutschool">ABOUT SN PUBLIC SCHOOL</h1>
+      <h1 className="aboutus">About Us</h1>
+      <hr className="line" />
       <div className="ourschool">
-      
         <h1 className="os_heading">OUR SCHOOL</h1>
         <div>
           <img className="school_img" src={img} />
         </div>
         <p className="os_content">
-          St. Michael’s High School, located at Digha Ghat in Patna, is a
-          Catholic Missionary educational instition, founded in 1858, by the
-          first Bishop of Patna, Dr. Anastasius Hartmann (of the Franciscan
-          (Capuchin) Missionary Society). Begun as a boarding school for poor
-          and orphan boys under the Catholic Chruch at Kurji, the School has
-          witnessed tremendous growth in terms of strength of students on its
-          rolls, and physical infrastructure. With over 3000 students (both boys
-          and girls) on its rolls today, the school is poised for further growth
-          as it moves into the 155th year of its founding. Since 1968, the
-          School is managed by the Patna Jesuit Society, the Bihar provincial
-          unit of the world-wide Catholic missionary organization called the
-          Society of Jesus (Jesuit Society). Founded in 1540 by St. Ignatius of
-          Loyola, the Society of Jesus has been active in the field of education
-          throughout the world since its origin. In India, the Society of Jesus
-          is presently running some 270 schools (Primary, middle and high), 80
-          higher secondary/Inter-colleges, 45 degree colleges catering to some
-          360,000 students belonging to different social classes, communities
-          and linguistic groups, through the medium of ten regional languages
-          and English. St. Michael’s High School is a Minority Charitable
-          Institution affiliated to the Central Board of Secondary Education for
-          the All India Secondary School Examination (Class X) and the All India
-          Senior School Certificate Examination (Class XII).
+          Welcome to SN Public School, a distinguished institution committed to
+          shaping the minds and futures of our young learners. Established with
+          a fervent dedication to education, SN Public School has emerged as a
+          beacon of excellence, providing unparalleled quality education for
+          students from LKG to VIII. Nestled in the tranquil surroundings of
+          Barka Gaon, Ara, Bihar, our school stands as a testament to academic
+          integrity, holistic development, and a nurturing environment. Our
+          sprawling campus is more than just a physical space; it's a community
+          where learning transcends the confines of classrooms. Here, every
+          child's potential is not just acknowledged but celebrated, and each
+          student is guided on a journey of self-discovery and academic
+          achievement.At SN Public School, we recognize that education is more
+          than the transfer of knowledge; it is the key to unlocking individual
+          potential and building a foundation for a successful and fulfilling
+          life. Our committed faculty members, who are not just teachers but
+          mentors, create an environment that fosters curiosity, critical
+          thinking, and a genuine love for learning.We take pride in providing
+          state-of-the-art facilities that support and enhance the learning
+          experience. From well-equipped classrooms to modern laboratories and a
+          library filled with a diverse range of resources, every aspect of our
+          infrastructure is designed to contribute to the overall growth of our
+          students.SN Public School is more than just an educational
+          institution; it is a community of learners, educators, and parents
+          working collaboratively to shape the future. We encourage open
+          communication and active involvement of parents in their child's
+          educational journey, believing that education is a shared
+          responsibility. As we embark on this journey of knowledge and growth
+          together, SN Public School remains dedicated to nurturing not only
+          students' academic prowess but also their character, values, and
+          passion for lifelong learning. Join us in creating a future where
+          every child's potential is not just recognized but fully realized.
         </p>
         <h1 className="os_heading">VISION & MISSION</h1>
         <div>
           <img className="school_img" src={vis} />
         </div>
         <p className="os_content">
-          Jesuit Education is inspired by the life and teachings of Jesus
-          Christ, the founder of Christianity. It is also based on the
-          principles of CHARACTER formation elaborated by St. lgnatius of
-          Loyola, the founder of the Society of Jesus. With Jesus Christ as our
-          model and rooted in the charism of the Society of Jesus, the Patna
-          Jesuits in their education ministry aim at the integral growth of
-          their students, forming them to be women and men for others, persons
-          of conscience, competence and compassionate commitment, and agents for
-          building a just and humane society. We believe in a Christian social
-          ethic which demands justice as an absolute requirement of faith in God
-          as our Father and Mother and the acceptance of each other as brothers
-          and sisters in God’s sight. The vision and the principles give Jesuit
-          Educational Institutions a distinct character and set before the staff
-          and the students high ideals of humanism and service. St. Michael’s
-          High School is a Minority Charitable Institution affiliated to the
-          Central Board of Secondary Education for the All India Secondary
-          School Examination (Class X) and the All India Senior School
-          Certificate Examination (Class XII).
+          Our vision at SN Public School is to be a trailblazer in education,
+          setting new standards for excellence and innovation. We aspire to
+          create a dynamic learning environment that not only imparts knowledge
+          but also instills values, preparing students to face the challenges of
+          the globalized world with confidence.Our mission is rooted in the
+          belief that education is a transformative journey. We are dedicated to
+          providing a well-rounded education that goes beyond textbooks. Through
+          a blend of academic rigor, character development, and extracurricular
+          opportunities, we aim to empower students to become compassionate,
+          responsible, and forward-thinking individuals.At SN Public School, our
+          mission is to provide a comprehensive and enriching educational
+          experience. We are committed to: Academic Excellence: Fostering a love
+          for learning through a curriculum aligned with CBSE standards,
+          promoting critical thinking and problem-solving skills. Character
+          Development: Instilling values such as integrity, empathy, and
+          respect, shaping students into socially responsible citizens. Holistic
+          Growth: Nurturing the physical, emotional, and mental well-being of
+          every student, recognizing and encouraging individual talents. Global
+          Citizenship: Preparing students for the challenges of a globalized
+          world by promoting cultural understanding and a sense of social
+          responsibility.
         </p>
         <h1 className="os_heading">OUR MISSION</h1>
         <p className="os_content">
-          The Jesuit-run St. Michael’s High School thus aims at making its own
-          contribution towards a radical transformation of the present-day
-          society so that the principles of social justice, equality of
-          opportunity, genuine freedom and respect for the religious and moral
-          values enshrined in the Constitution of India may prevail and the
-          possibility of living a fully human existence may be open to all
-          Indians. Our mission is to form young men and women for others, who
-          will distinguish themselves by their academic excellence, sound
-          character, spirit of selfless services, and leadership qualities. They
-          will commit themselves to the task of ushering in the much needed
-          radical transformation of present day social conditions and, thus,
-          contribute towards building a just and eco-friendly human society
-          where genuine freedom, equality of opportunities and respect for
-          religious, moral and social values exist. We do all this with the aid
-          of a committed, interactive and disciplined teaching-learning process
-          based on the methodology evolved by our Founder, St. lgnatius of
-          Loyola (lgnatian Pedagogical Paradigm-IPP), with its inter-play of
-          context, experience, reflection, action and evaluation, and by
-          creating an environment of trustful, open and friendly relationships
-          in our Educational Institutions.
+          Welcome to the vibrant tapestry of learning and growth at SN Public
+          School. More than just an educational institution, we are a close-knit
+          community that celebrates the uniqueness of every student. Established
+          with a deep-seated passion for learning, SN Public School stands tall
+          as a symbol of educational excellence in the heart of Barka Gaon, Ara,
+          Bihar.Our commitment to fostering a love for learning goes beyond
+          conventional methods. We provide diverse learning opportunities that
+          cater to various learning styles, ensuring that each student can
+          explore their interests and passions. From interactive classroom
+          sessions to hands-on projects and collaborative learning experiences,
+          we aim to make education a dynamic and engaging journey.At SN Public
+          School, we understand that each child is unique, possessing their own
+          strengths and areas for growth. Our dedicated faculty ensures that
+          students receive individualized attention, creating an environment
+          where questions are encouraged, and every challenge is seen as an
+          opportunity for personal development.In an era of interconnectedness,
+          we recognize the importance of preparing students for a globalized
+          world. Our curriculum incorporates a global perspective, exposing
+          students to diverse cultures, ideas, and issues. We believe in
+          nurturing global citizens who are not only academically proficient but
+          also socially aware and culturally sensitive.We believe in the power
+          of collaboration between the school and parents. Our approach
+          encourages active parental involvement in a child's educational
+          journey. Regular parent-teacher interactions, workshops, and events
+          create a supportive network where the school and parents work hand in
+          hand to provide the best possible learning environment for each
+          student.
         </p>
         <h1 className="os_heading">AIMS & OBJECTIVES</h1>
         <p className="os_content">
-          As Jesuit educators our effort is to develop our students into a fully
-          human, fully Indian and truly modern person. We want our students to
-          be intellectually competent, open to growth, to be religious and
-          loving committed to faith and justice. We facilitate our students to
-          realize the sense of worth and dignity of themselves and of others as
-          human persons, especially of those whose human rights and dignity are
-          violated and in need of liberation and protection. We strive towards
-          the all-round integral developments of each student, a sound
-          intellectual formation that develops all the cognitive, imaginative,
-          affective, creative and spiritual dimensions of each student. In the
-          integral development of student we give due emphasis to
-          extra-curricular activities such as sports, inter and intra school
-          tournaments, arts, music, debates etc. Implied in our education
-          ministry is the task to go out into those frontiers of education where
-          children are deprived of education due to various reasons. We pledge
-          to eradicate illiteracy from the face of Bihar in a systematic and
-          sustained manner. We, in collaboration with other’s, develop in our
-          students a sense of personal as well as corporate responsibility for
-          the family, the society, the nation and the world. This will be
-          expressed and exercised in the living out their chosen careers and
-          vocations.
+          Embedded in the ethos of SN Public School are our steadfast aims and
+          objectives that guide our educational endeavors. Our foremost
+          objective is to provide diverse learning opportunities that transcend
+          traditional boundaries, ensuring that each student discovers and
+          nurtures their unique talents and interests. We are committed to
+          offering individualized attention, recognizing that every child is
+          distinct and deserving of personalized support, fostering an
+          environment where questions are welcomed, and challenges become
+          stepping stones for personal growth. A global perspective is
+          interwoven into our curriculum, aiming to prepare students for a world
+          that is intricately connected. We aspire to cultivate global citizens
+          who not only excel academically but also possess a keen understanding
+          of diverse cultures, ideas, and global issues. Parental involvement is
+          paramount in our educational philosophy, fostering a collaborative
+          partnership between the school and parents. Regular interactions,
+          workshops, and events create a supportive network where collective
+          efforts shape a conducive learning environment. Beyond the confines of
+          academics, SN Public School places a significant emphasis on
+          extracurricular activities, recognizing the importance of a holistic
+          development approach. Our students are encouraged to participate in
+          sports, arts, music, and community service, contributing to their
+          well-rounded growth and fostering a sense of responsibility and
+          teamwork. Inclusivity is a cornerstone of our values, and our aim is
+          to provide an environment where diversity is not only acknowledged but
+          celebrated. SN Public School strives to create an inclusive space
+          where every student feels a sense of belonging, appreciating and
+          embracing differences as integral components of our vibrant school
+          community.
         </p>
       </div>
-      </div>
+    </div>
   );
 };
 export default About;
